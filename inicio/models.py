dd408e5e2302c566d1c2353383c1d5e19eaa9d3c
Juan Jose Bascuñan <jubascunanc@udd.cl>
2023-11-08 00:05:17 -0400

    config template

--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,11 @@
+
+# Create your models here.
 from django.db import models
 
 # Create your models here.
+class Auto(models.Model):
+
+    marca = models.CharField(max_length=30)
+    modelo = models.CharField(max_length=30)
+    anio= models.IntegerField()
+    
--- inicio/views.py
+++ inicio/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+
+def inicio(request):
+
+    return render(request,"mi_template.html")
+    
+ 
